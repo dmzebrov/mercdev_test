288e31649d47da5fb9bf0832962a4d2ee84e0f12
dmzebrov <dmzebrov@gmail.com>
2019-09-06 20:06:20 +0400

    Button touch effect fixed

--- index.js
+++ index.js
@@ -60,6 +60,19 @@ var app = {
   init: function() {
     document.getElementById("email").value = "";
     document.getElementById("password").value = "";
+    this.addButtonHoverEffect();
+  },
+
+  addButtonHoverEffect: function() {
+    var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+
+    if (!touchsupport) {
+        var buttons = document.getElementsByClassName("button");
+
+        for (let i = 0; i < buttons.length; i++) {
+          buttons[i].className += " button_button-hover";
+        };
+    }
   },
 
   updateAppState: function(updateType) {
