49da792ea163f91aaae06ff55f5d9e5984b0fa05
dmzebrov <dmzebrov@gmail.com>
2019-09-06 20:14:02 +0400

    Prettied

--- index.js
+++ index.js
@@ -64,14 +64,17 @@ var app = {
   },
 
   addButtonHoverEffect: function() {
-    var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+    var touchsupport =
+      "ontouchstart" in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0;
 
     if (!touchsupport) {
-        var buttons = document.getElementsByClassName("button");
+      var buttons = document.getElementsByClassName("button");
 
-        for (let i = 0; i < buttons.length; i++) {
-          buttons[i].className += " button_button-hover";
-        };
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].className += " button_button-hover";
+      }
     }
   },
 
